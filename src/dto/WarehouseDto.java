feeb554f46026f147e421f1eaf358f990e0874f9
seonmin12 <seonmink12@naver.com>
2025-03-14 16:28:32 +0900

    addDto

--- src/dto/WarehouseDto.java
+++ src/dto/WarehouseDto.java
@@ -1,4 +1,17 @@
 package dto;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+
 public class WarehouseDto {
+
+    private String ware_id;
+    private String client_id;
+    private String prod_id;
+    private int quantity;
+    private Date last_inbound_day;
+    private Date last_outbount_day;
 }
