be98b6885777ca71e968f837cb3c43321d534d87
donghwi <cksael@gmail.com>
2025-03-16 14:22:06 +0900

    ValidCheck - 회원가입 양식에 필요한 정규식 정의.
    SignUp에 필요한 각각의 Controller, Repository, Service 정의

---
+++ src/UserManagement/controller/SignUpController.java
@@ -0,0 +1,8 @@
+package UserManagement.controller;
+
+import dto.UserDto;
+
+public interface SignUpController {
+
+    boolean signUp(UserDto userDto);
+}
---
+++ src/UserManagement/repository/SignUpRepositoryImpl.java
@@ -0,0 +1,4 @@
+package UserManagement.repository;
+
+public class SignUpRepositoryImpl {
+}
