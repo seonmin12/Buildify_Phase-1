3ac2c4748dd5508d291b93ab542fa0527eaff2dc
donghwi <cksael@gmail.com>
2025-03-16 20:11:07 +0900

    회원가입 시 user가 id를 입력했을 때, 이미 중복된 ID가 있는지 검출 후 회원가입 진행

--- src/domain/DH_UserManagement/controller/SignUpController.java
+++ src/domain/DH_UserManagement/controller/SignUpController.java
@@ -4,5 +4,9 @@ import dto.UserDto;
 
 public interface SignUpController {
 
-    boolean signUp(UserDto userDto);
+    //회원가입
+    boolean signUp();
+
+    //중복된 ID 있는지 체크
+    boolean duplicateCheckUserID(String userid);
 }
