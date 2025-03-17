e783e63431b81f884a64255bd70a4f22c2c1947d
dlwjdtn1112 <dlwjdtn0103@naver.com>
2025-03-17 12:51:30 +0900

    "회원이 충관리자에게 출고 요청을 하는 기능 각 controller service repositoary 함수 선언을 했습니다 아직 기능 구현을 안했습니다."

--- src/domain/Outbound/repository/OutboundMemberRequestRepositoryImpl.java
+++ src/domain/Outbound/repository/OutboundMemberRequestRepositoryImpl.java
@@ -2,5 +2,5 @@ package domain.Outbound.repository;
 
 public class OutboundMemberRequestRepositoryImpl implements OutboundmemberRequestRepository{
     //회원이 총관리자에게 승인 요청을 하는 기능
-    //회원이 관리자에게 출고 요청한다
+    //회원이 관리자에게 출고 요구한다
 }
