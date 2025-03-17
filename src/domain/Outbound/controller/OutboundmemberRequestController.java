d7827ea6fb5827b4d35efa9bf28a6efcfc0d6371
dlwjdtn1112 <dlwjdtn0103@naver.com>
2025-03-17 12:47:05 +0900

    "회원이 충관리자에게 출고 요청을 하는 기능 각 controller service repositoary 함수 선언을 했습니다 아직 기능 구현을 안했습니다."

--- src/domain/Outbound/controller/OutboundmemberRequestController.java
+++ src/domain/Outbound/controller/OutboundmemberRequestController.java
@@ -1,5 +1,6 @@
 package domain.Outbound.controller;
 
 public interface OutboundmemberRequestController {
+    //회원이 관리자에게 출고 요청
     private void processShippingRequest(){}
 }
--- src/domain/Outbound/service/OutboundMemberRequsetServiceImpl.java
+++ src/domain/Outbound/service/OutboundMemberRequsetServiceImpl.java
@@ -1,4 +1,5 @@
 package domain.Outbound.service;
 
 public class OutboundMemberRequsetServiceImpl implements OutboundMemberRequsetService {
+    //회원이 관리자에게 출고 요청
 }
