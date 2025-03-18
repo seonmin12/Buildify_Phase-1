b391aac0f8668da76a3ea1dd6e2a2dee47bc851c
seonmin12 <seonmink12@naver.com>
2025-03-18 11:28:15 +0900

    회원 clientID로 출고 정보 조회하는 기능 추가

--- src/domain/Outbound/repository/OutboundUserRepositoryImpl.java
+++ src/domain/Outbound/repository/OutboundUserRepositoryImpl.java
@@ -1,7 +1,57 @@
 package domain.Outbound.repository;
 
+import common.ErrorCode;
+import config.DBConnection;
+import dto.OutboundDto;
+import exception.OutboundException;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * (고객용) 출고관리 Repository 구현체입니다.
  */
 public class OutboundUserRepositoryImpl implements OutboundUserRepository {
+    Connection connection = DBConnection.getConnection();
+    CallableStatement cs = null;
+    ResultSet rs = null;
+    PreparedStatement pstmt = null;
+
+    @Override
+    public List<OutboundDto> outboundUserRead(String clientID) throws OutboundException {
+        try {
+            Connection connection = DBConnection.getConnection();
+            List<OutboundDto> outboundDtos = new ArrayList<>();
+
+            connection.setAutoCommit(false);
+            cs = connection.prepareCall("{call outbound_user_read(?)}");
+            cs.setString(1,clientID);
+            rs = cs.executeQuery();
+
+            while(rs.next()){
+                OutboundDto outboundDto = OutboundDto.builder()
+                        .outbound_number(rs.getString("key"))
+                        .prod_id(rs.getString("prod_id"))
+                        .client_id(rs.getString("client_id"))
+                        .quantity(rs.getInt("quantity"))
+                        .outbound_status(rs.getInt("status"))
+                        .req_outbound_day(rs.getDate("req_outbound_day"))
+                        .ware_id(rs.getString("ware_id"))
+
+                        .build();
+
+                outboundDtos.add(outboundDto);
+
+            }
+            cs.close();
+            return outboundDtos;
+
+        } catch (SQLException e) {
+           e.printStackTrace();
+           throw new OutboundException(ErrorCode.ERROR_INPUT);
+        }
+
+
+    }
 }
--- src/domain/Outbound/service/OutboundUserServiceImpl.java
+++ src/domain/Outbound/service/OutboundUserServiceImpl.java
@@ -1,7 +1,32 @@
 package domain.Outbound.service;
 
+import common.ErrorCode;
+import domain.Outbound.repository.OutboundUserRepository;
+import domain.Outbound.repository.OutboundUserRepositoryImpl;
+import dto.OutboundDto;
+import exception.OutboundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * (관리자용) 출고관리 Service 구현체입니다.
  */
-public class OutboundUserServiceImpl {
+public class OutboundUserServiceImpl implements OutboundUserService {
+    private OutboundUserRepository outboundUserRepository;
+
+    public OutboundUserServiceImpl(OutboundUserRepository outboundUserRepository) {
+        this.outboundUserRepository = outboundUserRepository;
+    }
+
+    @Override
+    public List<OutboundDto> outboundUserRead(String clientID) throws OutboundException {
+        List<OutboundDto> outboundlist = outboundUserRepository.outboundUserRead(clientID);
+
+        if(outboundlist == null || outboundlist.isEmpty()){
+            throw new OutboundException(ErrorCode.DB_READ_ALL_ERROR);
+        }
+        return outboundlist;
+
+    }
 }
